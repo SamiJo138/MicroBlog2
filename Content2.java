fd3a12e65d36f820464bdb324f6b0d3df5e4eba6
Sami Jo Cook <Samijo138@gmail.com>
2017-12-13 19:19:02 -0600

    Update 2.o

--- Content2.java
+++ Content2.java
@@ -1,16 +1,23 @@
 
-class Content {
+public class Content2 {
 
-    private String content;
-    private String userPost;
+    private int postNum;
+    private String newPost;
+    private String currentUser;
+    private String website;
 
-public String getContent()
-        {
-            return content + "\n" +userPost;
-        }
-        Content(String postContent, String userPosted)
-        {
-            this.content=postContent;
-            this.userPost=userPosted;
-        }
+    Content2(int postNum, String newPost, String currentUser, String website) {
+
+        this.postNum = postNum;
+        this.newPost = newPost;
+        this.currentUser = currentUser;
+        this.website = website;
+    }
+
+
+    public String getContent2Info() {
+        return this.postNum + ": " + this.currentUser + "\n" + this.newPost;
+
+    }
+
 }
