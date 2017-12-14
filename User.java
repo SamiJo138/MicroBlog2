3dc10f8c2112d66712a584492786c6e40569aa21
Sami Jo Cook <Samijo138@gmail.com>
2017-12-13 19:19:57 -0600

    Update 2.o

--- User.java
+++ User.java
@@ -1,24 +1,51 @@
+import java.util.ArrayList;
 
-import java.util.Scanner;
-
-public class User {
+public class User2 {
 
     private String name;
-private String screenName;
-private String url;
-private String email;
-static Scanner keyboard=new Scanner(System.in);
-private int userID;
-public String getUserInfo()
-        {
-            return name+"\n" +screenName+"\n"+url+ "\n"+email;
-        }
-        User(String firstLast, String userName, String urlPic, String emailAdd)
-        {
-            this.name=firstLast;
-            this.screenName=userName;
-            this.url=urlPic;
-            this.email=emailAdd;
-        }
+    private String screenName;
+    private String url;
+    private String email;
+    private ArrayList<Content2> posts;
 
+    User2(String name, String screenName, String url, String email)
+    {
+        this.name=name;
+        this.screenName=screenName;
+        this.url=url;
+        this.email=email;
+        this.posts=new ArrayList<>();
+    }
+
+    public void addPost(Content2 post)
+    {
+        this.posts.add(post);
+    }
+
+    public String getUser2Info()
+
+    {
+        return this.name+"\n" +this.screenName+"\n"+this.url+ "\n"+this.email;
+    }
+
+
+    public String getScreenName()
+    {
+        return this.screenName;
+    }
+
+    public String getUserName()
+    {
+        return this.name;
+    }
+
+    public ArrayList<Content2> getPosts()
+    {
+        return this.posts;
+    }
+
+    public Content2 getLatestPost()
+    {
+        return this.posts.get(this.posts.size()-1);
+    }
 }
