97c29391b5eb3e8c82d478d3a4dd2d272d04d386
Sami Jo Cook <Samijo138@gmail.com>
2017-12-13 19:17:26 -0600

    Update 2.o

--- MicroBlog2.java
+++ MicroBlog2.java
@@ -1,92 +1,127 @@
-import java.util.ArrayList;
 import static java.lang.System.out;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class MicroBlog2
-{
-    Scanner keyboard=new Scanner(System.in);
+class MicroBlog2 {
 
-    public static void main(String[]args)
-    {
+    Scanner keyboard = new Scanner(System.in);
+
+    public static void main(String[] args) {
         int option;
-        Scanner keyboard=new Scanner(System.in);
-        boolean blogging=true;
-        ArrayList<User>allUsers= new ArrayList<User>();
-        ArrayList<Content>createdPosts=new ArrayList<Content>();
+        Scanner keyboard = new Scanner(System.in);
+        boolean blogging = true;
 
-        String currentUser="";
-        int i=0;
+        ArrayList<User2> allUsers = new ArrayList<>();
+
+        allUsers.add(new User2("Cory Spruce","PhoenixRising","https://goo.gl/images/9MhB5i","CSpruce777@gmail.com"));
+        allUsers.add(new User2("Jessie Andes","AliceForest","https://goo.gl/images/9uCRAd","Jandes@yahoo.com"));
+        allUsers.add(new User2("Robert Terwilliger","SideshowBob","https://goo.gl/images/rGOXMb","MurderBart123@sbcglobal.com"));
+
+        ArrayList<Content2> createdPosts = new ArrayList<>();
+
+        User2 currentUser = null;
+        int i;
+
+        int postNum=0;
 
         do {
-            printMenu();
+
+            printMenu(currentUser);
             option = keyboard.nextInt();
             keyboard.skip("\n");
-            if(option<1||option>6)
-            {
-                out.println("I'm sorry that is an invalid option.\n");
-            }
+            switch (option) {
 
-            if (option == 1) {
-                out.println("Since you are a new user, please enter your first and last name.");
-                String name = keyboard.nextLine();
-                out.println("Please choose a screen name.");
-                String screenName=keyboard.nextLine();
-                out.println("Please select a url as the picture for your profile.");
-                String url = keyboard.nextLine();
-                out.println("Please enter your email address.");
-                String emailAdd = keyboard.nextLine();
+                case 1:
+                    out.println("Since you are a new user, please enter your first and last name.");
+                    String name = keyboard.nextLine();
+                    out.println("Please choose a screen name.");
+                    String screenName = keyboard.nextLine();
+                    out.println("Please select a url as the picture for your profile.");
+                    String url = keyboard.nextLine();
+                    out.println("Please enter your email address.");
+                    String email = keyboard.nextLine();
 
-                allUsers.add(new User(name,screenName, url,emailAdd));
-                out.println("Thanks! Now you may log in from the menu.");
+                    currentUser = new User2(name, screenName, url, email);
+                    allUsers.add(currentUser);
+                    out.println("Thanks! Now you may log in from the menu.");
+                    break;
 
-            }
-            if (option == 2) {
-                out.println("If you are an existing user, please enter the number that corresponds to your username.");
-                for (i = 0; i < allUsers.size(); i++) {
-                    out.println((i + 1) + "\n" + allUsers.get(i).getUserInfo() + "\n");
+                case 2:
 
-                }
-                int userSelect = keyboard.nextInt();
-                currentUser = allUsers.get(userSelect - 1).getUserInfo();
-                out.println("You are now the user:\n" + currentUser + "\n\n");
-            }
-
-                if (option == 3)
-                {
-                if(currentUser.equals(""))
-                {
-                    out.println("I'm sorry, you need to become a user before you can make a post!");
-                }
-                    out.println("To enter your post, just type a message and hit enter.");
-                    String newPost = keyboard.nextLine();
-                    createdPosts.add(new Content(newPost, currentUser));
-                }
-                if (option == 4)
-                {
-                    out.println("Here is the list of the posts in Microblog2\n");
-                    for (i = 0; i < createdPosts.size(); i++) {
-                        out.println("\n" + createdPosts.get(i).getContent() + "\n");
-                    }
-                }
-                if (option == 5)
-                {
-                    out.print("Here is a current list of all users who have signed up with MicroBlog2\n");
+                    out.println("If you are an existing user, please enter the number that corresponds to your username.");
                     for (i = 0; i < allUsers.size(); i++) {
-                        out.println("" + allUsers.get(i).getUserInfo() + "\n");
+                        out.println((i + 1) + "\n" + allUsers.get(i).getUser2Info() + "\n");
+
                     }
-                }
-                if (option == 6)
-                {
-                    out.println("Goodbye " +currentUser);
+                    int userSelect = keyboard.nextInt();
+                    if (allUsers.size() >= (userSelect - 1)) {
+                        currentUser = allUsers.get(userSelect - 1);
+                        out.println("You are now the user:\n" + currentUser.getScreenName() + "\n\n");
+                    } else {
+                        out.println("Value invalid. Please choose an option between 1 and " + (allUsers.size() + 1));
+                    }
+                    if (allUsers.size() == 0) {
+                        out.println("There are no existing users. Please create one.");
+                    }
+                    break;
+
+                case 3:
+                    if (currentUser == null) {
+                        out.println("I'm sorry, you need to become a user before you can make a post! Please create an account.");
+                    } else {
+                        if (!currentUser.getPosts().isEmpty()) {
+                            out.println(currentUser.getLatestPost().getContent2Info());
+                            out.println("\n\n");
+                        }
+                        out.println("To enter your post, just type a message and hit enter.");
+                        String newPost = keyboard.nextLine();
+                        String website = "";
+
+                        Content2 savedPost = new Content2(postNum, newPost, currentUser.getScreenName(), website);
+                        currentUser.addPost(savedPost);
+                        createdPosts.add(savedPost);
+                        postNum++;
+                    }
+
+                    break;
+
+                case 4:
+                    out.println("Here is the list of the posts in Microblog2:\n");
+                    for (i = 0; i < createdPosts.size(); i++) {
+                        out.println(""+createdPosts.get(i).getContent2Info()+ "\n");
+                    }
+                    break;
+
+                case 5:
+                    out.print("Here is a current list of all users who have signed up with MicroBlog2:\n");
+                    for (i = 0; i < allUsers.size(); i++) {
+                        out.println("" + allUsers.get(i).getUser2Info()+ "\n");
+                    }
+                    break;
+
+                case 6:
+                    out.println("Goodbye " + currentUser.getScreenName());
                     blogging = false;
-                }
+                    break;
+                default:
+                    out.println("I'm sorry that is an invalid option. Try again.\n");
+                    printMenu(currentUser);
+
+                    break;
+
             }
-            while (blogging) ;
+        }
+        while (blogging);
+    }
+
+
+    public static void printMenu(User2 currentUser) {
+        if(currentUser != null){
+            out.println("Welcome to MicroBlog2, " + currentUser.getUserName() + "!\nMAIN MENU");
+        } else {
+            out.println("Welcome to MicroBlog2\nMAIN MENU");
         }
 
-    public static void printMenu()
-    {
-        out.println("Welcome to MicroBlog2\nMAIN MENU");
         out.println("1:Create a new user");
         out.println("2:Existing user");
         out.println("3:Create post as current user");
@@ -96,4 +131,6 @@ public class MicroBlog2
         out.println("What would you like to do?");
 
     }
+
+
 }
